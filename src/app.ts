75530ab6c5cec24a8bdfbfd66db466359e2c4c8a
Jelailah <anofiujelailah@gmail.com>
2021-12-17 10:43:31 +0100

    detect metamask installation

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import WalletConnect from "walletconnect";
 import { providers, ethers } from "ethers";
+import detectEthereumProvider from "@metamask/detect-provider";
 
 const btn = document.getElementById('btn') as HTMLButtonElement;
 let address = document.getElementById('daddress') as HTMLInputElement;
@@ -7,38 +8,76 @@ let addressVal = address.value;
 let amount = document.getElementById('damount') as HTMLInputElement;
 let amountVal = amount.value;
 
-btn.onclick = async () => {
-    //  Create WalletConnect SDK instance
-    const wc = new WalletConnect();
+(window as any).getAccount = async () => {
+	const accounts = await (window as any).ethereum.request({ method: 'eth_requestAccounts' })
 
-    //  Connect session (triggers QR Code modal)
-    const connector = await wc.connect();
-
-    //  Get your desired provider
-
-    const provider = await wc.getWeb3Provider({
-        infuraId: "e93d42bc84a24d3eac4cd187495f0adf",
-        qrcodeModalOptions: {
-            mobileLinks: [
-            "rainbow",
-            "metamask",
-            "argent",
-            "trust",
-            "imtoken",
-            "pillar",
-            ],
+	const account = await accounts[0];
+    let weiAmount = ethers.utils.parseEther(amountVal);
+    console.log(weiAmount)
+	
+	await (window as any).ethereum
+    .request({
+      method: 'eth_sendTransaction',
+      params: [
+        {
+          from: accounts[0],
+          to: addressVal,
+          value: weiAmount._hex,
         },
+      ],
+    })
+    .then((txHash: any) => console.log(txHash))
+    .catch((error: any) => {
+        console.error;
+        let errorMessage = document.getElementById('error-message') as HTMLParagraphElement;
+        errorMessage.innerHTML = error.message;
     });
+}
 
-    const account = await provider.enable();
+btn.onclick = async () => {
+    const ethereumProvider = await detectEthereumProvider();
 
-    //  Wrap with Web3Provider from ethers.js
-    const web3Provider = new providers.Web3Provider(provider);
-    
-    
-    const signer = web3Provider.getSigner();
-      const tx = await signer.sendTransaction({
-        to: addressVal,
-        value: ethers.utils.parseEther(amountVal),
-      });
+    if (ethereumProvider) {
+        await (window as any).getAccount();
+    } else {
+        //  Create WalletConnect SDK instance
+        const wc = new WalletConnect();
+
+        //  Connect session (triggers QR Code modal)
+        const connector = await wc.connect();
+
+        //  Get your desired provider
+
+        const provider = await wc.getWeb3Provider({
+            infuraId: "e93d42bc84a24d3eac4cd187495f0adf",
+            qrcodeModalOptions: {
+                mobileLinks: [
+                "rainbow",
+                "metamask",
+                "argent",
+                "trust",
+                "imtoken",
+                "pillar",
+                ],
+            },
+        });
+
+        const account = await provider.enable();
+
+        //  Wrap with Web3Provider from ethers.js
+        const web3Provider = new providers.Web3Provider(provider);
+        
+        
+        const signer = web3Provider.getSigner();
+        await signer.sendTransaction({
+            to: addressVal,
+            value: ethers.utils.parseEther(amountVal),
+        })
+        .then((txHash: any) => console.log(txHash))
+        .catch((error: any) => {
+            console.error;
+            let errorMessage = document.getElementById('error-message') as HTMLParagraphElement;
+            errorMessage.innerHTML = error.message;
+        });
+    }
 }
